8c7e14d0c6d548a4eab4db9b6e0650e0eae1b87f
Chris Marshall <chris@codegoalie.com>
2020-02-19 07:58:11 -0500

    Add styled components and Header to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Header>
+        <Logo src={logo} className="App-logo" alt="logo" />
+        <HeaderMenu>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            About
+          </a>
+        </HeaderMenu>
+        <AppLinks>
+          <a href="/">Sign in</a>
+          <a href="/">Register</a>
+        </AppLinks>
+      </Header>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const Header = styled.div`
+  margin: 2rem auto;
+  width: 1200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 241px;
+`;
+
+const HeaderMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem;
+`;
+
+const AppContainer = styled.div`
+  width: 100vw;
+`;
+
+const AppLinks = styled.div`
+  position: absolute;
+  right: 2rem;
+
+  a {
+    margin-left: 1rem;
+  }
+`;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders sign in link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/sign in/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders register link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/register/i);
   expect(linkElement).toBeInTheDocument();
 });
