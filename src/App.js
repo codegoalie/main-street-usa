68542988e1fb0b8ca65f6f3b2286fe9cdab8c99f
Chris Marshall <chris@codegoalie.com>
2020-03-17 19:53:59 -0400

    Copy in stuff from other project

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
+    <AppContainer>
+      <Header>
+        <Logo src={logo} className="App-logo" alt="logo" />
+        <HeaderMenu>
           <a
             className="App-link"
-            href="https://reactjs.org/"
+            href="https://blog.lineleader.io"
             target="_blank"
             rel="noopener noreferrer"
           >
-            React
+            Blog
           </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+        </HeaderMenu>
+        <AppLinks>
+          <a href="/">Sign in</a>
+          <a href="/">Register</a>
+        </AppLinks>
+      </Header>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const Header = styled.div`
+  margin: 2rem auto;
+  width: 1200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 241px;
+`;
+
+const HeaderMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem;
+`;
+
+const AppContainer = styled.div`
+  width: 100vw;
+`;
+
+const AppLinks = styled.div`
+  position: absolute;
+  right: 2rem;
+
+  a {
+    margin-left: 1rem;
+  }
+`;
